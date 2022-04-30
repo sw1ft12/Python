de9eaa97aa9e8379423be6a474d6e0d11bc34761
sw1ft12 <99120105+sw1ft12@users.noreply.github.com>
2022-05-01 02:30:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,11 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, login_user
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Ad.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-#login_manager = LoginManager(app)
-
 
 class Ads(db.Model):
     __tablename__ = 'ads'
@@ -21,25 +17,7 @@ class Ads(db.Model):
 
     def __repr__(self):
         return '<Article %r>' % self.id
-
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    username = db.Column(db.String(50), nullable=False, unique=True)
-    password_hash = db.Column(db.String(100), nullable=False)
-    created_on = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return "<{}:{}>".format(self.id, self.username)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
+    
 
 @app.route('/')
 def index():
